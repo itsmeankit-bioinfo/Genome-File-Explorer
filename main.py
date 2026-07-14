c17842c6d99cb0fb3f908f6a0233c5b1cef2f02d
itsmeankit-bioinfo <simplyankitraj@gmail.com>
2026-07-14 13:32:54 +0530

    Add sequence statistics engine

---
+++ main.py
@@ -0,0 +1,37 @@
+from src.fasta import FASTAReader
+
+
+def main():
+
+    reader = FASTAReader("sample_data/sample.fasta")
+
+    sequences = reader.load()
+
+    print("=" * 55)
+    print("Genome File Explorer")
+    print("=" * 55)
+
+    for record in sequences:
+
+        seq = str(record.seq)
+
+        print(f"\nSequence ID : {record.id}")
+        print(f"Description : {record.description}")
+
+        print(f"\nLength       : {reader.sequence_length(seq)} bp")
+        print(f"GC Content   : {reader.gc_content(seq):.2f}%")
+        print(f"AT Content   : {reader.at_content(seq):.2f}%")
+
+        counts = reader.nucleotide_count(seq)
+
+        print(f"A Count      : {counts['A']}")
+        print(f"T Count      : {counts['T']}")
+        print(f"G Count      : {counts['G']}")
+        print(f"C Count      : {counts['C']}")
+        print(f"N Count      : {counts['N']}")
+
+        print("-" * 55)
+
+
+if __name__ == "__main__":
+    main()
--- src/fasta.py
+++ src/fasta.py
@@ -2,13 +2,42 @@ from Bio import SeqIO
 
 
 class FASTAReader:
-    """Read and analyze FASTA files."""
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.records = []
 
     def load(self):
-        """Load sequences from a FASTA file."""
         self.records = list(SeqIO.parse(self.filepath, "fasta"))
         return self.records
+
+    def sequence_length(self, sequence):
+        return len(sequence)
+
+    def gc_content(self, sequence):
+        sequence = sequence.upper()
+
+        g = sequence.count("G")
+        c = sequence.count("C")
+
+        return ((g + c) / len(sequence)) * 100
+
+    def at_content(self, sequence):
+        sequence = sequence.upper()
+
+        a = sequence.count("A")
+        t = sequence.count("T")
+
+        return ((a + t) / len(sequence)) * 100
+
+    def nucleotide_count(self, sequence):
+
+        sequence = sequence.upper()
+
+        return {
+            "A": sequence.count("A"),
+            "T": sequence.count("T"),
+            "G": sequence.count("G"),
+            "C": sequence.count("C"),
+            "N": sequence.count("N")
+        }
